34f425799ea4b53d822ab57c747012bd8b5c3257
Demid Mosko <demid.mosko@gmail.com>
2021-03-10 19:47:23 +0300

    OM v2 snapshoting (#2)

--- src/snapshot.ts
+++ src/snapshot.ts
@@ -4,6 +4,7 @@ import { snapshotOm } from "./snapshot_om";
 import { snapshotOmNft } from "./snapshot_om_nft";
 import { Contract, Snapshot } from "./snapshot_type";
 import { snapshotUni } from "./snapshot_uni";
+import { snapshotOm2 } from "./snapshot_om2";
 import { findBearingSnapshot } from "./utils";
 
 export async function buildSnapshot<T extends Contract>(
@@ -20,6 +21,8 @@ export async function buildSnapshot<T extends Contract>(
       return snapshotUni(web3, blockNumber, bearingSnapshot as Snapshot<Contract.UNI_OM_LP>) as Promise<Snapshot<T>>;
     case Contract.OM_NFT:
       return snapshotOmNft(web3, blockNumber, bearingSnapshot as Snapshot<Contract.OM_NFT>) as Promise<Snapshot<T>>;
+    case Contract.OM2:
+      return snapshotOm2(web3, blockNumber, bearingSnapshot as Snapshot<Contract.OM2>) as Promise<Snapshot<T>>;
     default: throw new Error(`Snapshoting "${contract}" contract not implemented`);
   }
 }
